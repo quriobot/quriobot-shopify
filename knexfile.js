38fd50aeb4661d9d5bb217b55e083e16b38a4792
Ubuntu <mrtienhp97@gmail.com>
2023-03-22 12:18:33 +0800

    replace host and port with env variable

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,12 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-console.log(process.env.MYSQL_DATABASE)
-
 module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
-      port : 3306,
+      host : process.env.MYSQL_HOST,
+      port : process.env.MYSQL_PORT,
       user : process.env.MYSQL_USERNAME,
       password : process.env.MYSQL_PASSWORD,
       database : process.env.MYSQL_DATABASE,
@@ -22,8 +20,8 @@ module.exports = {
   production: {
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
-      port : 3306,
+      host : process.env.MYSQL_HOST,
+      port : process.env.MYSQL_PORT,
       user : process.env.MYSQL_USERNAME,
       password : process.env.MYSQL_PASSWORD,
       database : process.env.MYSQL_DATABASE,
