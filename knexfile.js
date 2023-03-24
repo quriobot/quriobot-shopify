f796c0fe4ca7bb3b2c8171fe10eb9715f4833341
Ubuntu <mrtienhp97@gmail.com>
2023-03-24 23:11:11 +0800

    update docker

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,6 @@ module.exports = {
       user : process.env.MYSQL_USERNAME,
       password : process.env.MYSQL_PASSWORD,
       database : process.env.MYSQL_DATABASE,
-      socketPath: '/var/run/mysqld/mysqld.sock'
     }
   }
 };
